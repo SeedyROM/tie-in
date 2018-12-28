db37a805a2ff49aace9ac84a3e7fd611a154f2d2
Zack Kollar <rallokkcaz@gmail.com>
2018-12-28 02:14:45 -0800

    Wow, this is sort of working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
+// Initial assets and modules
 import logo from "../../assets/logo.svg";
 import "./App.scss";
 
+// styled-components hack
+// @ts-ignore
+const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!../styles/theme.scss'); // tslint:disable-line
+
+// Components
 import Routes from "../routes";
 import NavBar from "./ui/NavBar";
 
 class App extends Component {
   public render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="App__container">
-            <NavBar />
-            <Routes />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <div className="App__container">
+              <NavBar />
+              <Routes />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
